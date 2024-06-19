e46b18f4de0c33e3c4a73a718c123902ac6253ac
Viver Val Bungag <viverbungag1@gmail.com>
2024-06-19 19:52:27 +0800

    |FEATURE| updated the firstSection of the home page

--- frontend/src/components/main/firstSection/FirstSection.tsx
+++ frontend/src/components/main/firstSection/FirstSection.tsx
@@ -24,36 +24,45 @@ const FirstSection = () => {
           alt="Background Image"
           className="absolute w-screen sm:h-screen opacity-20"
         />
-        <div className="relative h-full xl:pb-16">
+        <div className="relative h-full pb-16">
           <div className="flex flex-col items-center justify-center w-full xl:h-full">
-            <div className="grid xl:grid-cols-2 grid-cols-1 p-8 max-w-[90rem] justify-center">
-              <div className="flex flex-col xl:items-start items-center gap-4 justify-self-center xl:pl-20 col-span-1">
-                <h1
-                  className={`${IntegralCF.className} text-primaryT relative sm:text-[4rem] text-[2rem] xl:text-left text-center leading-tight`}
-                >
-                  CUSTOM PRINTED
-                  <span className="block sm:text-[2rem] text-[1rem]">
-                    T<span className={Coolvetica.className}>-</span>SHIRTS{' '}
-                    <span className={Coolvetica.className}>&</span> APPAREL
-                  </span>
-                </h1>
-                <h2
-                  className={`${Coolvetica.className} text-white text-center xl:text-left sm:text-[1.2rem] xl:max-w-full max-w-[40rem]`}
-                >
-                  Use custom t-shirts, apparel and merch to ensure that you stay
-                  ahead of the competition. Together, we’ll have you wearing
-                  garments that has accurate prints, high quality control and a
-                  solid professional feel.
-                </h2>
-                <div className="flex flex-col sm:flex-row items-center gap-4">
-                  <Button styleType="primary">SHOP FOR PRODUCTS</Button>
-                  <Button styleType="secondary">REQUEST A QUOTE</Button>
+            <div className="flex flex-col">
+              <div className="flex flex-col xl:flex-row gap-0 xl:gap-12 p-8">
+                <div className="max-w-[40rem] flex flex-col xl:items-start items-center gap-4 justify-self-center col-span-1">
+                  <h1
+                    className={`${IntegralCF.className} text-primaryT relative sm:text-[4rem] text-[2rem] xl:text-left text-center leading-tight`}
+                  >
+                    CUSTOM PRINTED
+                    <span className="block sm:text-[2rem] text-[1rem]">
+                      T<span className={Coolvetica.className}>-</span>SHIRTS{' '}
+                      <span className={Coolvetica.className}>&</span> APPAREL
+                    </span>
+                  </h1>
+                  <h2
+                    className={`${Coolvetica.className} text-white text-center xl:text-left sm:text-[1.2rem] xl:max-w-full max-w-[40rem]`}
+                  >
+                    Use custom t-shirts, apparel and merch to ensure that you
+                    stay ahead of the competition. Together, we’ll have you
+                    wearing garments that has accurate prints, high quality
+                    control and a solid professional feel.
+                  </h2>
+                  <div className="flex flex-col sm:flex-row items-center gap-4">
+                    <Button styleType="primary">SHOP FOR PRODUCTS</Button>
+                    <Button styleType="secondary">REQUEST A QUOTE</Button>
+                  </div>
+                </div>
+                <div className="flex justify-center col-span-1 xl:mt-0 mt-8">
+                  {/* <CardCarousel contents={contents} /> */}
+                  <Image
+                    src={'/main/firstSection/carouselPic2.png'}
+                    width={300}
+                    height={300}
+                    alt="Review Picture"
+                    className="max-w-[20rem] rounded-md"
+                  />
                 </div>
               </div>
-              <div className="col-span-1 xl:mt-0 mt-8">
-                <CardCarousel contents={contents} />
-              </div>
-              <div className="flex items-center justify-center xl:col-span-2 col-span-1 mt-16">
+              <div className="flex items-center justify-center xl:col-span-2 col-span-1 mt-8">
                 <div className="flex flex-col sm:w-[50rem] w-[20rem] items-center justify-center gap-4 bg-[#F7F7F8] rounded-lg py-4 px-8">
                   <h3
                     className={`${Coolvetica.className} text-[1.3rem] text-black text-center`}
